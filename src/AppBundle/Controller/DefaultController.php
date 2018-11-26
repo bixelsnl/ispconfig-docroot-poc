eac83ab3c1e37d6e07376e06875f74200bf1fc1f
Choong Wei Tjeng <choong@bixels.nl>
2018-11-26 13:46:31 +0100

    initial commit

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function numberAction()
+    {
+        $number = mt_rand(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+}
